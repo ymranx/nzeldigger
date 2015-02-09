56a898ba9b85675ba362d226428a588ea6bc1088
ymranx <ymranx@gmail.com>
2015-02-10 00:45:40 +0530

    initial

---
+++ nzeldigger.js
@@ -0,0 +1,44 @@
+var request    = require('request'),
+    events     = require('events');
+	
+var nzelDigger = function(accessToken) {
+    var _this         = this;
+    this.accessToken  = accessToken;
+    this.page         = 1;
+    this.per_page     = 50;
+    this.baseJobUrl   ='https://api.angel.co/1/jobs';
+    this.lastPage     = 0;
+    var pageHandler   = requestJobList(this.baseJobUrl + '?page=1&per_page=1&access_token=' + this.accessToken);
+   
+    pageHandler(function(error, response, body) {
+      if(error) {
+         _this.emit('error', error);
+         return;
+      }
+       _this.lastPage = Math.ceil(JSON.parse(body).total/50);
+       _this.emit('ready');
+    });
+};
+
+nzelDigger.prototype.__proto__ = events.EventEmitter.prototype;
+
+nzelDigger.prototype.getJobs = function() {
+  var _this = this,
+	  curUrl= '';
+  return function*() {
+    while(_this.page <= 2/*_this.lastPage*/) {
+	curUrl = _this.baseJobUrl +'?page=' + _this.page++ + '&per_page=' + _this.per_page + '&access_token=' + _this.accessToken;
+        yield requestJobList(curUrl);
+    }
+  }
+};
+
+function requestJobList(jobUrl) {
+  return function(callback){
+    request(jobUrl, function(error, response, body){
+      callback(error, response, body);
+    });
+  }
+}
+
+module.exports = nzelDigger;
